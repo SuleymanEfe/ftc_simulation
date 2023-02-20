8d125e9fd8291811fe1aa67389a8341fbaad2116
Efe Celik <suleymanefeclk2@gmail.com>
2023-02-20 02:24:39 -0800

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform robot;
+
+    public Camera topViewCam;
+    public Camera robotViewCam;
+
+    private float maxDistance = 50f;
+    private float minDistance = 30f;
+    private float scrollSpeed = 5f;
+    private float distanceToTarget = 50;
+
+    private Vector3 previousPosition;
+
+    public void init(Transform robot)
+    {
+        this.robot = robot;
+
+        disableAllCameras();
+        robotViewCam.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            disableAllCameras();
+            robotViewCam.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            disableAllCameras();
+            topViewCam.enabled = true;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        distanceToTarget -= scroll * scrollSpeed;
+        distanceToTarget = Mathf.Clamp(distanceToTarget, minDistance, maxDistance);
+
+        robotViewCam.transform.position = robot.position;
+        robotViewCam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPosition = robotViewCam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 newPosition = robotViewCam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 direction = previousPosition - newPosition;
+
+            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+            robotViewCam.transform.position = robot.position;
+
+            robotViewCam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            robotViewCam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
+
+            robotViewCam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+            previousPosition = newPosition;
+        }
+    }
+
+
+    void disableAllCameras()
+    {
+        topViewCam.enabled = false;
+        robotViewCam.enabled = false;
+    }
+
+
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -10,78 +10,23 @@ public class Controller : MonoBehaviour
     [SerializeField] private UIController uiController;
 
     private DataParser dataParser;
+    private CameraController cameraController;
 
     [SerializeField] private GameObject mainMenuUI;
 
     public Transform robot;
 
-    public Camera topViewCam;
-    public Camera robotViewCam;
-
-    private float maxDistance = 50f;
-    private float minDistance = 30f;
-    private float scrollSpeed = 4f;
-    private float distanceToTarget = 30;
-
-    private Vector3 previousPosition;
-
     void Start()
     {
         dataParser = this.AddComponent<DataParser>();
+        cameraController = this.GetComponent<CameraController>();
 
-        pauseGame();
+        cameraController.init(robot);
 
-        disableAllCameras();
-        robotViewCam.enabled = true;
+        pauseSimulation();
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            disableAllCameras();
-            robotViewCam.enabled = true;
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            disableAllCameras();
-            topViewCam.enabled = true;
-        }
-
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-
-        distanceToTarget -= scroll * scrollSpeed;
-        distanceToTarget = Mathf.Clamp(distanceToTarget, minDistance, maxDistance);
-
-        robotViewCam.transform.position = robot.position;
-        robotViewCam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            previousPosition = robotViewCam.ScreenToViewportPoint(Input.mousePosition);
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 newPosition = robotViewCam.ScreenToViewportPoint(Input.mousePosition);
-            Vector3 direction = previousPosition - newPosition;
-
-            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
-            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
-
-            robotViewCam.transform.position = robot.position;
-
-            robotViewCam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
-            robotViewCam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
-
-            robotViewCam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
-
-            previousPosition = newPosition;
-        }
-    }
-
-    void disableAllCameras()
-    {
-        topViewCam.enabled = false;
-        robotViewCam.enabled = false;
-    }
-
-    public void startGame()
+    public void startSimulation()
     {
         mainMenuUI.SetActive(false);
 
@@ -96,13 +41,13 @@ public class Controller : MonoBehaviour
         robotController.startRobotMovement();
     }
 
-    public void pauseGame()
+    public void pauseSimulation()
     {
         Time.timeScale = 0;
         robotController.stopRobotMovement();
     }
 
-    public void continueGame()
+    public void continueSimulation()
     {
         Time.timeScale = 1;
         robotController.startRobotMovement();
